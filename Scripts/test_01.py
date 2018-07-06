385611026c46d6eb834c373c707ddad8e5a80ec8
ysug <18338862971@163.com>
2018-07-06 20:39:10 +0800

    updata

--- Scripts/test_01.py
+++ Scripts/test_01.py
@@ -1,5 +1,7 @@
 import allure
 class Test_01:
-    @allure.step(title="第一次测试")
+    @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
+    @allure.step(title="测试步骤001")
     def test_01(self):
+        allure.attach('描述', '我是测试步骤001的描述～～～')
         assert 0
